aa1fb94ec876327019314d65f398a57bdf8b1e2f
vodkar <boombarah@mail.ru>
2022-06-09 23:43:52 +0500

    Added topogy model

---
+++ server/yamt/hosts/models/topology.py
@@ -0,0 +1,20 @@
+from typing import Generic, TypeVar
+
+from yamt.common.model import YamtModel
+
+from .host import Host
+from .ip_interface import IPInterface
+
+T = TypeVar("T")
+
+
+class Connection(YamtModel, Generic[T]):
+    origin: T
+    destination: T
+
+
+class Topology(YamtModel):
+    interface_connections: list[Connection[IPInterface]]
+
+    def get_host_topology(self) -> Connection[Host]:
+        pass
